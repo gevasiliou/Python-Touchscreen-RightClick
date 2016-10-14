3aa65f60b3c7ebebf903430e2e43f9634b899307
George Vasiliou <ge.vasiliou@gmail.com>
2016-10-14 15:31:03 +0300

    Update Python_Touchscreen_RightClick.py

--- Python_Touchscreen_RightClick.py
+++ Python_Touchscreen_RightClick.py
@@ -1,20 +1,15 @@
 """
-Zyell's Python Script for adding 1 & 2 finger multitouch gestures to implement
+Python Script for adding 1 & 2 finger multitouch gestures to implement
 a right click option with Touchscreens in the Ubuntu unity environment.
-
 This is implemented with the evdev Python library on an ELAN touchscreen.
-
 Currently implements 2 types of right click options:
-1 finger long touch: Timeout of 0.7 seconds, movement cancels action
+1 finger long touch: Timeout of 1.7 seconds, movement cancels action
 2 finger tap: movement cancels action
-
-Modified by gevasiliou (GV) , October 2016
 """
 
-from evdev import InputDevice, ecodes, UInput, list_devices, AbsInfo #GV: AbsInfo imported
-from pymouse import PyMouse  #GV added
-import datetime
-
+from evdev import InputDevice, ecodes, UInput, list_devices
+from threading import Timer
+from pymouse import PyMouse #gv added.
 
 class TrackedEvent(object):
 
@@ -24,44 +19,52 @@ class TrackedEvent(object):
     timing of long presses, and event completion.
     """
 
-    def __init__(self):
+    def __init__(self, dev, abilities, var_x, var_y):
         """ Initialize tracking attributes. """
+        self.dev = dev
+        self.abilities = abilities
+        self.vars = {'ABS_X': var_x, 'ABS_Y': var_y}
         self.position = {'ABS_X': None, 'ABS_Y': None}
-        self.slots = []
         self.fingers = 0
         self.total_event_fingers = 0
         self.discard = 0
         self.moved = 0
         self.track_start = None
         self.click_delay = 0.7
+        self.grabbed=False
 
-    def add_finger(self, slot):
+    def add_finger(self):
         """  Add a detected finger. """
-        if slot not in self.slots:
-            self.fingers += 1
-            self.slots.append(slot)
-        if self.total_event_fingers < self.fingers:
-            self.total_event_fingers = self.fingers
+        self.fingers += 1
+        self.total_event_fingers = self.fingers
 
     def remove_fingers(self):
         """ Remove detected finger upon release. """
-        if self.total_event_fingers == self.fingers:
-            if self.total_event_fingers == 0:
-                self.total_event_fingers = 1
+        if self.fingers == 1:
             print('Total Fingers used: ', self.total_event_fingers)
         self.fingers -= 1
 
         if (self.fingers == 0 and
                 self.total_event_fingers == 2 and
                 self.moved == 0):
+            self.total_event_fingers = 0
             self._initiate_right_click()
 
-        elif ((self.fingers == 0 or self.fingers == -1) and
+        elif (self.fingers == 0 and
                 self.total_event_fingers == 1 and
                 self.moved == 0):
-            self._internal_timing()
+            self.total_event_fingers = 0
+            try:
+                self.track_start.cancel()
+                self.track_start.join()
+            except AttributeError:  # capture Nonetype track_start
+                pass
+            try:
+                self.dev.ungrab()
+            except (OSError,IOError):  # capture case where grab was never initiated
+                pass
 
-        if self.fingers == 0 or self.fingers == -1:
+        if self.fingers == 0:
             self.discard = 1
 
     def position_event(self, event_code, value):
@@ -69,57 +72,42 @@ class TrackedEvent(object):
         if self.position[event_code] is None:
             self.position[event_code] = value
         else:
-            OldValue = self.position[event_code]  #gv added
-            NewValue = value  #gv added
-            diff = OldValue - NewValue	#gv added
-            if abs(diff) > 50:  #gv added :allows a movement of +/- 50 pixels before to cancel right click 
-                print('movement more than treshold, cancelling right click')
+            print('touch offset:',abs(self.position[event_code] - value))
+            if abs(self.position[event_code] - value) > self.vars[event_code]:
                 self._moved_event()
+        if (self.fingers == 1 and self.position['ABS_X'] and
+                self.position['ABS_Y'] and self.track_start is None):
+            self._trackit()
 
-    def trackit(self):
+    def _trackit(self):
         """ start timing for long press """
-        self.track_start = datetime.datetime.now()
+        self.track_start = Timer(self.click_delay, self._long_press)
+        self.track_start.start()
+
+    def _long_press(self):
+        if self.fingers == 1 and self.moved == 0:
+            self._initiate_right_click()
+            self.dev.grab()
 
     def _moved_event(self):
         """ movement detected. """
         self.moved = 1
-
-    def _internal_timing(self):
-        """ Internal method for determining long press time right clicking. """
-        if self.track_start is not None:
-            elapsed = datetime.datetime.now() - self.track_start
-            if elapsed.total_seconds() > self.click_delay:
-                self._initiate_right_click()
+        print('moved too much')
 
     def _initiate_right_click(self):
         """ Internal method for initiating a right click at touch point. """
-        """
-        #Zyell Script - Original Code:
-        capabilities = {ecodes.EV_ABS: (ecodes.ABS_X, ecodes.ABS_Y),
-                        ecodes.EV_KEY: (ecodes.BTN_LEFT, ecodes.BTN_RIGHT)}
-        
-        # Code that worked for my setup
-        capabilities = {
-			ecodes.EV_KEY : [ecodes.BTN_LEFT, ecodes.BTN_RIGHT],
-			ecodes.EV_ABS : [(ecodes.ABS_X, AbsInfo(value=1900, min=0, max=3264, fuzz=0, flat=0, resolution=13)),
-				(ecodes.ABS_Y, AbsInfo(1050, 0, 1856, 0, 0, 13))]
-		}
-        # Values for min,max,fuzz,flat and resolution have been copied by my touch screen capabilities (running evtest)
-        
-        # Event injection bellow with ui.write works ok (applying modified device capabilities) but i decided to use pymouse.
-        with UInput(capabilities) as ui:
+        '''
+        with UInput(self.abilities) as ui:
             ui.write(ecodes.EV_ABS, ecodes.ABS_X, 0)
             ui.write(ecodes.EV_ABS, ecodes.ABS_Y, 0)
             ui.write(ecodes.EV_KEY, ecodes.BTN_RIGHT, 1)
             ui.write(ecodes.EV_KEY, ecodes.BTN_RIGHT, 0)
             ui.syn()
-            ui.close() # GV added - Maybe is not correct to close ui
-        """ 
-        m = PyMouse()  # gv: use of pymouse module
-        x, y = m.position()  # gv: gets mouse current position coordinates during click
-        #print('position:', x, y)  #gv: just for debugging
-        m.click(x, y, 2)  # gv: the third argument represents the mouse button click (1 left,2 right,3 middle)
-        # GV: Right click is sent to wherever your mouse pointer is on the time of click.
+        '''
+        m = PyMouse()
+        x , y = m.position()  # gets mouse current position coordinates
+        m.click(x, y, 2)  # the third argument represents the mouse button (1 click,2 right click,3 middle click)
+        print('Right Click injected')
 
 def initiate_gesture_find():
     """
@@ -129,33 +117,53 @@ def initiate_gesture_find():
     """
     for device in list_devices():
         dev = InputDevice(device)
-        if (dev.name == 'ELAN Touchscreen') or (dev.name == 'Atmel Atmel maXTouch Digitizer'):
+        if (dev.name == 'ELAN Touchscreen') or \
+           (dev.name == 'Atmel Atmel maXTouch Digitizer'):
             break
-    codes = dev.capabilities()
     Abs_events = {}
+    abilities = {ecodes.EV_ABS: [ecodes.ABS_X, ecodes.ABS_Y],
+                 ecodes.EV_KEY: (ecodes.BTN_LEFT, ecodes.BTN_RIGHT)}
+    # Assuming QHD screen on my Yoga 2 Pro as default for resolution measures
+    res_x = 13  # touch unit resolution # units/mm in x direction
+    res_y = 13  # touch unit resolution # units/mm in y direction
+    # would be weird if above resolutions differed, but will treat generically
+    codes = dev.capabilities()
     for code in codes:
         if code == 3:
             for type_code in codes[code]:
-                Abs_events[type_code[0]] = ecodes.ABS[type_code[0]]
+
+                human_code = ecodes.ABS[type_code[0]]
+                if human_code == 'ABS_X':
+                    vals = type_code[1]
+                    abilities[ecodes.EV_ABS][0] = (ecodes.ABS_X, vals)
+                    res_x = vals[-1]
+                elif human_code == 'ABS_Y':
+                    vals = type_code[1]
+                    abilities[ecodes.EV_ABS][1] = (ecodes.ABS_Y, vals)
+                    res_y = vals[-1]
+                Abs_events[type_code[0]] = human_code
+    # Average  index finger width is 16-20 mm, assume 20 mm
+    # touch resolution noise assumed at 10% (5% radius), so 1.0 mm by default
+    # this seemed resonable from my own trial tests
+    var_x = 4.0 * res_x  # variablity in movement allowed in x direction
+    var_y = 4.0 * res_y  # variablity in movement allowed in y direction
 
     MT_event = None
     for event in dev.read_loop():
-        if MT_event:
-            if MT_event.discard == 1:
-                MT_event = None
         if event.type == ecodes.EV_ABS:
             if MT_event is None:
-                MT_event = TrackedEvent()
+                MT_event = TrackedEvent(dev, abilities, var_x, var_y)
             event_code = Abs_events[event.code]
-            if event_code == 'ABS_MT_SLOT':
-                MT_event.add_finger(event.value)
-            elif event_code == 'ABS_X' or event_code == 'ABS_Y':
+            if event_code == 'ABS_X' or event_code == 'ABS_Y':
                 MT_event.position_event(event_code, event.value)
             elif event_code == 'ABS_MT_TRACKING_ID':
                 if event.value == -1:
                     MT_event.remove_fingers()
+                    if MT_event.discard == 1:
+                        MT_event = None
                 else:
-                    MT_event.trackit()
+                    MT_event.add_finger()
+
 
 if __name__ == '__main__':
     initiate_gesture_find()
